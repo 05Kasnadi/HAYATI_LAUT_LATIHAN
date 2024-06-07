59af6c6ef55193d1462d14140c7696176b4906ed
05Kass <kasnadi@teknokrat.ac.id>
2024-06-07 09:58:07 +0700

    master :buat projek laravel

---
+++ backend/app/Models/hayati.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class hayati extends Model
+{
+    use HasFactory;
+
+     // buat variabel untuk inisialisasi tabel
+     protected $table = "hayati_latihan";
+}
